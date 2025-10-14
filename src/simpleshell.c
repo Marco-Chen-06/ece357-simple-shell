497b0c5276b0402a93da7b94674a651efa71697c
Marco Chen <marcochen.work@gmail.com>
2025-10-14 18:18:58 -0400

    implemented top quarter of flowchart

--- src/simpleshell.c
+++ src/simpleshell.c
@@ -1,4 +1,38 @@
+#include "simpleshell.h"
 
 int main(int argc, char *argv[]) {
-    
+    FILE* fptr = NULL;
+    char line_buf[BUFSIZ]; // lines are not expected to be greater than BUFSIZ bytes
+    int latest_exit_status = 0; // stores exit status value of last spawned command
+    char *token_ptr = NULL;
+    printf("Argc: %d", argc);
+    if (argc > 1) {
+        // more than 1 argument implies our shell is being called through an interpreter
+        if ((fptr = fopen(argv[1], "r")) == NULL) {
+            fprintf(stderr, "Failed to fopen file: %s. %s \n", argv[1], strerror(errno));
+            exit(1);
+        }
+    } else {
+        if((fptr = fdopen(0, "r")) == NULL) {
+            fprintf(stderr, "Failed to fdopen standard input. %s", strerror(errno));
+            exit(1);
+        }
+    }
+
+    for (;;) {
+        if (fgets(line_buf, sizeof(line_buf), fptr) == NULL) {
+            if (feof(fptr) != 0) {
+                // EOF detected, exit using status value of last spawned command
+                exit(latest_exit_status);
+            }
+            fprintf(stderr, "Failed to read line using fgets. %s", strerror(errno));
+            continue;
+        }
+        token_ptr = strtok(line_buf, " ");
+        while (token_ptr != NULL) {
+            printf("Argument detected: %s \n", line_buf);
+            token_ptr = strtok(NULL, " ");
+        }
+    }
+
 }
--- src/simpleshell.h
+++ src/simpleshell.h
@@ -1,6 +1,10 @@
 #ifndef SIMPLESHELL_H
 #define SIMPLESHELL_H
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
 
 
 
